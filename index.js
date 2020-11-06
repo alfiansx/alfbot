35a2aafb142d0b910699bf770466bed10eac916d
M Wisnu Maulana <73579250+re-Destro@users.noreply.github.com>
2020-11-06 17:18:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ fs.existsSync('./session.json') && conn.loadAuthInfo('./session.json')
 //conn.connectOptions.agent = ProxyAgent ('http://1.0.180.120:8080')
 conn.connect();
 
-conn.on('user-presence-update', json => console.log(json.id + 'sedang => ' + json.type)) || console.log('Bot by ig:@mwmaulana310')
+conn.on('user-presence-update', json => console.log(json.id + ' presence is => ' + json.type)) || console.log('Bot by ig:@mwmaulana310')
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
