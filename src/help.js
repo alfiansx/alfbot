dbfadafc298dcbf6dcf857299e40e64cfb4aaba7
alfx <39555780+alfiansx@users.noreply.github.com>
2020-12-28 01:59:24 +0700

    💢Refactoring Code💢

--- src/help.js
+++ src/help.js
@@ -9,32 +9,10 @@ usage : reply image, or send image with caption\n
 command : *${prefix}toimg*
 desc : convert sticker to image
 usage : reply sticker\n
-command : *${prefix}tsticker* or *${prefix}tstiker*
-desc : convert text to sticker
-usage : *${prefix}tsticker text in here*\n
-> *Meme Commands* <
-command : *${prefix}meme*
-desc : random meme images [english]
-usage : just send the command\n
-command : *${prefix}memeindo*
-desc : random meme images [indo]
-usage : just send the command\n
 > *Others Commands* <
-command : *${prefix}gtts*
+command : *${prefix}tts*
 desc : convert text to speech/audio
-usage : *${prefix}gtts [cc] [text]*\nexample : *${prefix}gtts ja On2-chan*\n
-command : *${prefix}loli*
-desc : random loli images
-usage : just send the command\n
-command : *${prefix}nsfwloli*
-desc : random nsfw loli images
-usage : just send the command\n
-command : *${prefix}url2img*
-desc : take web screenshots
-usage : *${prefix}url2img [tipe] [url]*\n
-command : *${prefix}simi*
-desc : your message will be replied to by simi
-usage : *${prefix}simi yourmessage*\n
+usage : *${prefix}tts [cc] [text]*\nexample : *${prefix}tts ja On2-chan*\n
 command : *${prefix}ocr*
 desc : take the text in the picture
 usage : reply image, or send image with caption\n
@@ -53,10 +31,6 @@ note : can only be used when the bot becomes admin, and the one who sends the co
 command : *${prefix}tagall*
 desc : tags all group members including admins too
 usage : just send the command
-note : This command can be used if you are a group admin\n
-command : *${prefix}simih*
-desc : activate simi mode in the group
-usage : *${prefix}simih 1* to activate simi mode and *${prefix}simih 0* to deactivate simi mode
 note : This command can be used if you are a group admin\n`
 }
 
