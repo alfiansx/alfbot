eb813ea1621b7d967843e9f91526857e41d5c788
alfx <39555780+alfiansx@users.noreply.github.com>
2020-12-28 01:57:19 +0700

    💢Refactoring Code💢

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ apt-get install tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 
-echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
+echo "[*] All dependencies have been install, please run the command \"npm start\" to immediately start the script"
